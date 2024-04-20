4d660b4cf8acff3ba0e0563fffa595a61fe35374
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-21 02:30:10 +0530

    styled heading and description

--- src/app/components/LandingContainer.tsx
+++ src/app/components/LandingContainer.tsx
@@ -6,17 +6,25 @@ const LandingContainer = () => {
     <div className="bg-white flex flex-col container max-w-[750px]">
       <div className="flex flex-row justify-between">
         <div className="basis-2/3">
-          <div>Vinay Badgujar</div>
+          <h1 className="text-3xl font-bold mb-2">Vinay Badgujar</h1>
           <div>
+            <span className="flex items-center text-sm font-light text-gray-400 me-3 mb-2">
+              <span className="flex w-2.5 h-2.5 bg-teal-500 rounded-full me-1.5 flex-shrink-0"></span>
+              Available for small to middle sized projects
+            </span>
+          </div>
+          <p className="text-md font-semibold">
             Hey there! I'm a Full Stack Developer who loves to build cool web
             apps. Check out my work and let's create something awesome together!
-          </div>
+          </p>
         </div>
-        <Image src="" alt="" className="w-[150px] h-[150px] rounded-full bg-slate-500" />
+        <Image
+          src=""
+          alt=""
+          className="w-[150px] h-[150px] rounded-full bg-slate-500"
+        />
       </div>
-      <div>
-        Nav bar
-      </div>
+      <div>Nav bar</div>
     </div>
   );
 };
