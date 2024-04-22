9d9f42a65c907aba149dfeb1b53a0fa0e2835864
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-04-22 21:47:56 +0530

    remove dark styles

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -8,7 +8,7 @@ interface ProjectCardProps {
     techStack: string[];
 }
 
-const ProjectCard = ({title, description, image, techStack}) => {
+const ProjectCard = ({title, description, image, techStack}: ProjectCardProps) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
       <a href="#">
@@ -20,16 +20,16 @@ const ProjectCard = ({title, description, image, techStack}) => {
       </a>
       <div className="p-5">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
             {title}
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700 ">
           {description}
         </p>
         <a
           href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
         >
           Read more
           <svg
