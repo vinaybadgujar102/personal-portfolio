d18e070ae9ebd7f0d5a12b16b676d06bf4d5804a
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-04 21:06:16 +0530

    make button component more general

---
+++ src/app/components/common/Button.tsx
@@ -0,0 +1,15 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+const Button = ({ children, key, className }: { children: React.ReactNode; key: string; className?: string }) => {
+  return (
+    <button
+      className={cn("border-2 font-medium py-2 px-4 rounded-3xl w-28 hover:bg-slate-100 hover:border-0", className)}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
