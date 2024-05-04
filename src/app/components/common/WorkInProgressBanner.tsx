86bded6ddfcc95c0c4cb85bc9d9ac4ba9dd373ca
vinaybadgujar102 <vinaybadgujar8@gmail.com>
2024-05-04 21:54:28 +0530

    add work in progress banner

---
+++ src/app/components/common/WorkInProgressBanner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkInProgressBanner = () => {
+  return <div className="border border-red-500 text-center">!!! Work In Progress !!!</div>;
+};
+
+export default WorkInProgressBanner;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import WorkInProgressBanner from "./components/common/WorkInProgressBanner";
 
 const poppins = Poppins({ weight: ["400", "500", "600", "700", "800", "900"], subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {" "}
+        <WorkInProgressBanner />
+        {children}
+      </body>
     </html>
   );
 }
